dc684b5d773a151fe511dcd855678cf767726b60
Sujey Lopez <sujey.le99@gmail.com>
2024-08-14 13:29:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        520: {
+            slidesPerView: 2
+        },
+        950: {
+            slidesPerView: 3
+        }
+    }
+});
+
